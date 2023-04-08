4d493362115c4c95fcc2451c72da0a6d4adeeafa
BwBest <raffine49@gmail.com>
2023-04-08 10:25:48 +0300

    Add equal button functionalty and fix logic problems

--- calculator.js
+++ calculator.js
@@ -1,8 +1,7 @@
-let firstOperand;
-let secondOperand;
-let sum;
+let firstOperand = 0;
 let displayValue = "";
 let chosenOperator;
+let finishedCalculating;
 
 const keyReferences = [];
 
@@ -19,60 +18,141 @@ const equalBtn = document.querySelector('#equal');
 const removeBtn = document.querySelector('#remove');
 const clearAllBtn = document.querySelector('#clear-all');
 
-
-
-function operate(num1, num2, operator){
-    switch(operator){
-        case "add":
-            add(num1,num2);
-            break;
-        case "subtract":
-            subtract(num1,num2);
-            break;
-        case "divide":
-            divide(num1,num2);
-            break;
-        case "multiply":
-            multiply(num1,num2);
-            break;
-        default:
-            break;
-    }
+//INITALIZE
+function init(){
+    firstOperand = 0;
+    displayValue = '';
+    chosenOperator = undefined;
+    finishedCalculating = 0;
+    updateCurrentText();
+    updateOperationText('');
 }
 
-function add(num1,num2){
-    return num1+num2;
-}
-function subtract(num1,num2){
-    return num1-num2;
-}
-function divide(num1,num2){
-    return num1/num2;
-}
-function multiply(num1,num2){
-    return num1*num2;
-}
-
+//GETTING VALUE FROM USER
 function updateCurrentValue(e){
     //e.target.id.slice(-1)
+    if(displayValue === '' && e.target.id.slice(-1) === '0') { return; }
+    if(finishedCalculating === 1)
+    {
+        init();
+    }
     displayValue += e.target.id.slice(-1);
-    updateScreen();
+    updateCurrentText();
 }
 
-function updateScreen(){
+//SCREEN UPDATING AND CLEARING
+function updateCurrentText(){
     mainText.textContent = displayValue;
+    if(mainText.textContent === '') {mainText.textContent = '0';} //Dont let user delete the default 0
 }
 
-function clear(){
+function updateOperationText(string){
+    operationText.textContent = string;
+}
+
+function clearAll(){
+    displayValue = '';
+    firstOperand = 0;
+    secondOperand = 0;
+    mainText.textContent = '0';
+    operationText.textContent = '';
+}
+
+function clearCurrentInput(){
     displayValue = '';
     mainText.textContent = '0';
 }
 
 function removeChar(){
     displayValue = displayValue.slice(0, displayValue.length-1);
-    updateScreen();
+    updateCurrentText();
 }
 
+//OPERATOR BUTTONS
+function plusButton(){
+    chosenOperator = '+';
+    finishedCalculating = 0;
+    if(firstOperand !== 0){
+        firstOperand = firstOperand + Number(displayValue);
+    } else {
+    firstOperand = Number(displayValue);
+    }
+    updateOperationText(`${firstOperand} +`);
+    clearCurrentInput();
+}
+
+function minusButton(){
+    chosenOperator = '-';
+    finishedCalculating = 0;
+    if(firstOperand !== 0){
+        firstOperand = firstOperand - Number(displayValue);
+    } else {
+    firstOperand = Number(displayValue);
+    }
+    updateOperationText(`${firstOperand} -`);
+    clearCurrentInput();
+}
+
+function divideButton(){
+    chosenOperator = '÷';
+    finishedCalculating = 0;
+    if(firstOperand !== 0){
+        if(displayValue === ''){
+            updateOperationText(`${firstOperand} ÷`);
+            return;
+        }
+        firstOperand = Math.round(firstOperand / Number(displayValue) * 10) / 10;
+    } else {
+    firstOperand = Number(displayValue);
+    }
+    updateOperationText(`${firstOperand} ÷`);
+    clearCurrentInput();
+}
+
+function multiplyButton(){
+    chosenOperator = '*';
+    finishedCalculating = 0;
+    if(firstOperand !== 0){
+        if(displayValue === ''){
+            updateOperationText(`${firstOperand} *`);
+            return;
+        }
+        firstOperand = firstOperand * Number(displayValue);
+    } else {
+    firstOperand = Number(displayValue);
+    }
+    updateOperationText(`${firstOperand} *`);
+    clearCurrentInput();
+}
+
+function equalButton(){
+    let sum;
+    switch(chosenOperator){
+        case '+':
+            sum = firstOperand + Number(displayValue);
+            break;
+        case '-':
+            sum = firstOperand - Number(displayValue);
+            break;
+        case '÷':
+            sum = Math.round(firstOperand / Number(displayValue) * 10) / 10;
+            break;
+        case '*':
+            sum = firstOperand * Number(displayValue);
+            break;
+        default:
+            break;
+    }
+    updateOperationText(`${firstOperand} ${chosenOperator} ${Number(displayValue)} =`);
+    displayValue = sum;
+    updateCurrentText();
+    finishedCalculating = 1;
+
+    firstOperand = 0; // We zero this value because we want to get display value(sum) to be first operand if user decides to continue calculating based on sum
+}
+
+
+//EVENT LISTENERS
 // Get the num-key references and add event listeners
 for(let i=0; i<10;i++){
     keyReferences.push(document.querySelector(`#key${i}`));
@@ -83,5 +163,10 @@ for(let key of keyReferences){
 }
 
 // Event listeners for operators and clear-remove
-clearAllBtn.addEventListener('click', clear);
+clearAllBtn.addEventListener('click', clearAll);
 removeBtn.addEventListener('click', removeChar);
+addBtn.addEventListener('click', plusButton);
+subtractBtn.addEventListener('click', minusButton);
+multiplyBtn.addEventListener('click', multiplyButton);
+divideBtn.addEventListener('click', divideButton);
+equalBtn.addEventListener('click', equalButton);
